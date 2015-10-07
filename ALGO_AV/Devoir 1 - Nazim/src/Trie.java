e569842bccc415928f607d2a2c660b198757ec27
Nazim Lachter <lachtater@gmail.com>
2015-10-07 15:13:11 +0200

    On avance : 3) Parcours en largeur

--- ALGO_AV/Devoir 1 - Nazim/src/Trie.java
+++ ALGO_AV/Devoir 1 - Nazim/src/Trie.java
@@ -1,20 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collection;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Trie {
 
 	private Node root;
+	Collection<Node> nodeTries = new LinkedList<Node>();
+
 	Trie(String filename) throws FileNotFoundException {
 		root = new Node(null, '.');
 		
 		Iterable<String> dictionary;
 		dictionary = BuildDic(filename);
 		for(String word: dictionary) addWord(word);
+	}
+	
+	public String toString() {			
+		return "ROOT : "+root.toString()
+			+"\nLOOKUP : "+nodeTries.toString();
 	}
 	
 	@SuppressWarnings("unused")
@@ -34,6 +43,12 @@ public class Trie {
 		public void showAllDico() {
 			Set<Character> children = this.ChildrenTable.keySet();
 			System.out.println(children);
+		}
+		
+		public String toString() {
+			return "Value : "+value
+					+"\tisInDico : "+isInDico
+					+"\tFather : "+father;
 		}
 	}
 	
@@ -56,6 +71,25 @@ public class Trie {
 		}
 	}
 	
+	public void lookup() {
+		Queue<Node> nodeQueue = new LinkedList<Node>();
+		nodeQueue.add(root);
+		
+		Node currentNode;
+		
+		while(nodeQueue.isEmpty()) {
+			currentNode = nodeQueue.remove();
+			
+			for(@SuppressWarnings("rawtypes") Map.Entry mapEntry : currentNode.ChildrenTable.entrySet()) {
+				nodeQueue.add(currentNode.ChildrenTable.get(mapEntry.getKey()));
+			}
+			
+			((LinkedList<Node>) nodeTries).addLast(currentNode);
+			
+			System.out.println(currentNode.value+"-----------\t");
+		}
+	}
+	
 	private static Iterable<String> BuildDic(String filename) throws FileNotFoundException {
 		Scanner scanner = new Scanner(new File(filename));
 		LinkedList<String> dictionary = new LinkedList<>();
@@ -66,9 +100,11 @@ public class Trie {
 		return dictionary;
 	}
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Trie trie  = new Trie("1256-0.txt");
-		trie.root.showAllDico();
+		//trie.root.showAllDico();
+		trie.lookup();
+		System.out.println(trie.toString());
 	}
 	
 }
