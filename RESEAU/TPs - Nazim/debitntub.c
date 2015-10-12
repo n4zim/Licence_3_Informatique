3615cf7e75c70f07a0e8992a6321215d008d6087
Nazim Lachter <lachtater@gmail.com>
2015-10-12 15:32:08 +0200

    Bricolage

--- RESEAU/TPs - Nazim/debitntub.c
+++ RESEAU/TPs - Nazim/debitntub.c
@@ -6,9 +6,21 @@
 #include <sys/time.h>
 #include <errno.h>
 
+/*
+ * TP3 : Scrutation
+ * 
+ * Ex1 : debitntub
+ * Prend une taille de buffer et un nombre de tubes en paramètres
+ * 
+ * Nazim Lachter
+*/
+
+#define NB_TUBES 100
+char *s;
+
 /* --------------- FONCTIONS DE DEBITUB.C ------------------ */
 int sigaction(int sig, const struct sigaction *p_action, struct sigaction *p_action_ancien);
-unsigned long n = 0;
+unsigned long n = 0; // Taille
 int signalType(int sig, void (*h)(int)) {
 	struct sigaction s;
 	s.sa_handler = h;
@@ -20,31 +32,32 @@ void capte(int sig) {
 	if(sig == SIGPIPE) { printf("Arrêt du tube\n"); exit(1); }
 	else if(sig == SIGALRM) { printf("Lecture par le fils : %d ko\n", n/1024); alarm(1); }
 	else { printf("Signal recu : %d\n", sig); }
+	
+	printf("DEBUG : %s\n", *s); //for(debug = 0; debug < sizeof(*s; debug++) printf("%s\t", *s[debug]);
 }
 /* --------------------------------------------------------- */
 
-#define NB_TUBES 100
-
 int main(int argc, char *argv[]) {
-	char *s;
-	int tubes[NB_TUBES][2];
-	int bufsize, nbtubes, pid, cpt;
-
 	if(argc != 3) { printf("Usage : ./debitntub <bufsize> <nbtubes>\n"); exit(0); }
+	
+	int tubes[NB_TUBES][2];
+	int bufsize, nbtubes;
+	unsigned cpt; // Compteur des boucles for
 
-	signalType(SIGPIPE, capte);
+	signalType(SIGPIPE, capte); // Création d'un signal
 	bufsize = atoi(argv[1]);
 	nbtubes = atoi(argv[2]);
 
 	if(nbtubes > NB_TUBES) { printf("Le nombre de tubes soit être inférieur ou égal à %d !\n", NB_TUBES); exit(0); }
 
-	s = malloc(bufsize * sizeof *s); // (Taille du tableau de char = taille du buffer)
+	s = malloc(bufsize * sizeof *s); // Taille du tableau de char = taille du buffer
 	if(!s) { perror("MALLOC"); exit(1); }
 
 	// Création de l'ensemble des pipes
 	for(cpt = 0; cpt < nbtubes; cpt++) if(pipe(tubes[cpt]) < 0) { perror("PIPE"); exit(1); }
-  
-	pid = fork();
+
+	int pid;
+	pid = fork(); // Création d'un fils
 	if(pid < 0) { perror("FORK"); exit(1); }
 	
 	if(pid == 0) { /* FILS */
@@ -53,10 +66,10 @@ int main(int argc, char *argv[]) {
 		
 		signalType(SIGALRM, capte);
 		alarm(1);
-		
+				
 		for(cpt = 0; cpt < nbtubes; cpt++) close(tubes[cpt][1]); // Femeture des sorties inutilisées
 		
-		while(1) {
+		while(1) {			
 			FD_ZERO(&set);
 			for(cpt = 0; cpt<nbtubes; cpt++) FD_SET(tubes[cpt][0], &set); // Ajout de l'ensemble des descriptions
 			
@@ -66,7 +79,7 @@ int main(int argc, char *argv[]) {
 					if(FD_ISSET(tubes[cpt][0], &set)) {
 						k = read(tubes[cpt][0], s, bufsize);
 						if(k < 0) { perror("READ"); exit(1); }
-						n += k;
+						n += k; // Ajout des nouveaux octets (nombre de caractères)
 					}
 				}
 			
@@ -94,7 +107,7 @@ int main(int argc, char *argv[]) {
 			if(res > 0) {
 				for(cpt = 0; cpt < nbtubes; cpt++)
 					if(FD_ISSET(tubes[cpt][1], &set))
-						if(write(tubes[cpt][1],s,bufsize) < 0) { perror("WRITE"); exit(1); }
+						if(write(tubes[cpt][1], s, bufsize) < 0) { perror("WRITE"); exit(1); }
 			
 			} else { perror("SELECT"); }
 		}
