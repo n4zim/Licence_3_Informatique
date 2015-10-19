23b2b13ca828052a6fc4c10c0bd7df0097cf745b
Nazim Lachter <lachtater@gmail.com>
2015-10-19 21:58:55 +0200

    Return des lignes

--- ALGO_AV/Devoir 2 - Filtres de Bloom (NM)/src/Person.java
+++ ALGO_AV/Devoir 2 - Filtres de Bloom (NM)/src/Person.java
@@ -5,7 +5,7 @@ public class Person {
 	String prenom; 
 	String ville;
 	
-	public Person(String prenom, String nom, String ville){
+	public Person(String prenom, String nom, String ville) {
 		this.prenom = prenom;
 		this.nom = nom; 
 		this.ville = ville;
@@ -23,28 +23,24 @@ public class Person {
 	
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		if(this == obj) return true;
+		if(obj == null) return false;
+		
+		if(getClass() != obj.getClass()) return false;
+		
 		Person other = (Person) obj;
 		if (nom == null) {
-			if (other.nom != null)
-				return false;
-		} else if (!nom.equals(other.nom))
-			return false;
+			if (other.nom != null) return false;
+		} else if (!nom.equals(other.nom)) return false;
+		
 		if (prenom == null) {
-			if (other.prenom != null)
-				return false;
-		} else if (!prenom.equals(other.prenom))
-			return false;
+			if (other.prenom != null) return false;
+		} else if (!prenom.equals(other.prenom)) return false;
+		
 		if (ville == null) {
-			if (other.ville != null)
-				return false;
-		} else if (!ville.equals(other.ville))
-			return false;
+			if (other.ville != null) return false;
+		} else if (!ville.equals(other.ville)) return false;
+		
 		return true;
 	} 
 	
