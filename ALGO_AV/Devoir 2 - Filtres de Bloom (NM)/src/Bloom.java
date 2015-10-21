40201be56ca03558c0a5f3536702c22afa6c22ce
Nazim Lachter <lachtater@gmail.com>
2015-10-21 15:01:38 +0200

    Maëva : Ebauche test HashSet ToDo : Séparé les positifs des faux positifs et réaliser le test avec les hashSet et treeSet

--- ALGO_AV/Devoir 2 - Filtres de Bloom (NM)/src/Bloom.java
+++ ALGO_AV/Devoir 2 - Filtres de Bloom (NM)/src/Bloom.java
@@ -3,9 +3,11 @@ import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Bloom<Key> {
 	
@@ -19,14 +21,19 @@ public class Bloom<Key> {
 	private Random gen;	  
 	
 	private  BitSet bitmap;
+	private HashSet hashset;
+	private TreeSet treeset;
+	
+	
 	
 	public Bloom(int numberOfKeys) {
 		this.gen = new Random();
 		
 		this.n = numberOfKeys;	
+		this.prime = BigInteger.probablePrime(30, gen).intValue(); // prime number generate
 		this.m = (int) Math.ceil((n * Math.log(p)) / Math.log(1.0 / (Math.pow(2.0, Math.log(2.0)))));
 		this.k = (int) Math.round(Math.log(2.0) * m / n); 
-		this.prime = BigInteger.probablePrime(30, gen).intValue(); // prime number generate 
+		 
 		this.pairs = new int[k][2];
 		
 		for(int i = 0; i < k; i++){
@@ -46,12 +53,20 @@ public class Bloom<Key> {
 	
 	public boolean probablyContains(Key key) {
 		int hash = key.hashCode();
-		
+			
 		for(int i=0; i<k; i++)
 			if(!bitmap.get(Math.abs((pairs[i][0]*hash+pairs[i][1])%prime%m)))
 				return false;
 		
+		//System.out.println(key.toString());
+		
 		return true;
+	}
+	
+	public boolean testHashSet(Key key){
+		Person liste = getPersonListFromFile();
+		for(int i = 0; i < liste)
+		return true; 
 	}
 	
 	public ArrayList<Person> getPersonListFromFile(String file) throws FileNotFoundException {	
@@ -66,20 +81,29 @@ public class Bloom<Key> {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		Bloom<Person> filter = new Bloom<Person>(577);
+		long start = System.nanoTime();
 		
+		Bloom<Person> filter = new Bloom<Person>(577);
+		HashSet<Person> filter2 = new HashSet<Person>();
+ 		
 		// --------------> Suspects <--------------
 		ArrayList<Person> suspectsList = filter.getPersonListFromFile("liste_suspects_577.txt");
 		Iterator<Person> it1 = suspectsList.iterator();
 		while(it1.hasNext()) filter.add(it1.next());
 		
+		ArrayList<Person> suspectsList2 = filter2.getPersonListFromFile("liste_suspects_577.txt");
+		Iterator<Person> it12 = suspectsList2.iterator();
+		while(it1.hasNext()) filter2.add(it12.next());
+		
 		// --------------> Liste globale <--------------
-		ArrayList<Person> globalList = filter.getPersonListFromFile("person_list_100k.txt");
-		Iterator<Person> it2 = globalList.iterator();
-
 		int compteur = 0;
+		ArrayList<Person> globalList = filter.getPersonListFromFile("person_list_100k.txt");
+		Iterator<Person> it2 = globalList.iterator();		
 		while(it2.hasNext()) if(filter.probablyContains(it2.next())) compteur++;
 		System.out.println("Nombre : " + compteur);
+		
+		long end = System.nanoTime()-start;
+		System.out.println("Temps d'éxécution : "+ end + " ns (" + (double)end/1000000000 + " s)");
 	}
 
 }
